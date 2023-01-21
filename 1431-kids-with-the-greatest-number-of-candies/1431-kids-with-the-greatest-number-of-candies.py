31a16161c456b8d11ebf3e15d042ae946826e2b5
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2023-01-21 23:31:25 +0530

    Time: 42 ms (68.02%), Space: 13.8 MB (58.94%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.py
@@ -0,0 +1,8 @@
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        l1=[]
+        for i in range(len(candies)):
+            total = candies[i]+extraCandies
+            l1.append(total >= max(candies))
+            
+        return l1
