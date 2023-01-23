908cb049dd8eb0c8b0aaacf7579b63f99ca8436f
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2023-01-23 22:24:42 +0530

    Time: 787 ms (54.57%), Space: 19 MB (59.07%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+
+        arr = [0] * (n+1)
+        for a ,b in trust:
+            arr[a]-=1
+            arr[b]+=1
+        
+        for i in range(1,n+1):
+            if arr[i] == n-1:
+                return i
+        return -1
+        
+        
+        
+        
