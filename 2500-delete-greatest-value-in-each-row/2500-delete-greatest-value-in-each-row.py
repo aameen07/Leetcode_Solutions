caa90094317c03409817f4f307bf5e5aa49dcfe5
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2023-01-24 22:40:04 +0530

    Time: 179 ms (46.08%), Space: 13.9 MB (57.97%) - LeetHub

--- 2500-delete-greatest-value-in-each-row/2500-delete-greatest-value-in-each-row.py
+++ 2500-delete-greatest-value-in-each-row/2500-delete-greatest-value-in-each-row.py
@@ -1,13 +1,10 @@
 class Solution:
     def deleteGreatestValue(self, grid: List[List[int]]) -> int:
         
-        m = len(grid)
-        n = len(grid[0])
-        
         res=0
-        for _ in range(n):
+        for _ in range(len(grid[0])):
             maxx=0
-            for i in range(m):
+            for i in range(len(grid)):
                 a=max(grid[i])
                 maxx = max(a,maxx)
                 grid[i].remove(a)
