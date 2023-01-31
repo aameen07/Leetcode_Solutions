5423befd171469de23bc3cb6f7d275790e4d8055
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2023-01-31 22:26:53 +0530

    Time: 28 ms (88.99%), Space: 14 MB (12.52%) - LeetHub

--- 1816-truncate-sentence/1816-truncate-sentence.py
+++ 1816-truncate-sentence/1816-truncate-sentence.py
@@ -1,10 +1,5 @@
 class Solution:
     def truncateSentence(self, s: str, k: int) -> str:
         
-        a=""
-        string2=[]
-        string1= s.split(" ")
-        for i in range(k):
-            string2.append(string1[i])
-        string1=" ".join(string2)
-        return (string1)
+        a=s.split(" ")
+        return " ".join(a[0:k])
