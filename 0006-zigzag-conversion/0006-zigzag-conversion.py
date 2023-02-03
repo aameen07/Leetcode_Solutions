3315d28ab5ba896cf8993b8d589ab8397de234af
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2023-02-03 22:42:48 +0530

    Time: 65 ms (63.43%), Space: 13.9 MB (94.57%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -0,0 +1,22 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        
+        if (numRows<2):
+            return s
+        
+        arr=[""]*numRows
+        direction="down"
+        row = 0
+        for i in s:
+            arr[row]+=i
+            if row == numRows-1:
+                direction = "up"
+            elif row == 0:
+                direction = "down"
+            if (direction == "down"):
+                row+=1
+            else:
+                row-=1
+        
+        return ("".join(arr))
+    
