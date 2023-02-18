b8dde96bb864667e94ea9ec0427e6c91b41ed692
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2023-02-18 23:12:18 +0530

    Time: 33 ms (88.51%), Space: 13.8 MB (94.55%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,19 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        
+        l=0
+        r=x
+        
+        while(l<=r):
+            
+            m=(l+r)//2
+            
+            if (m*m)<=x and ((m+1)*(m+1))>x:
+                return m
+            
+            elif (m*m) > x:
+                r=m-1
+                
+            else:
+                l=m+1
+        
