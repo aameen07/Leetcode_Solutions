950d1a52e9d58666de440682cb74e2657f14be68
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2023-02-25 16:08:22 +0530

    Time: 42 ms (57.18%), Space: 13.9 MB (47.29%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,19 +1,33 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
         
+        # l=0
+        # r=x
+        # while(l<=r):
+        #     m=(l+r)//2
+        #     if (m*m)<=x and ((m+1)*(m+1))>x:
+        #         return m
+        #     elif (m*m) > x:
+        #         r=m-1        
+        #     else:
+        #         l=m+1
+        
+        
+        
         l=0
         r=x
         
-        while(l<=r):
-            
+        if x==0 or x==1: return x
+        
+        while(l<r):
             m=(l+r)//2
             
-            if (m*m)<=x and ((m+1)*(m+1))>x:
-                return m
-            
-            elif (m*m) > x:
-                r=m-1
-                
+            if (m*m) <= x:
+                l=m+1    
             else:
-                l=m+1
+                r=m
         
+        return l-1
+    
+    
+        #   r=m-1, r=m, l=m+1, return m  ye 4 hi scenerio honge kisi condition ke andr finally 95% questions mei
