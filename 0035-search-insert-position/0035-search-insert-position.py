b913cdd2c153995a5445db2a6b459922e7213346
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2023-02-25 16:35:30 +0530

    Time: 43 ms (96.70%), Space: 14.8 MB (30.78%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,6 +1,22 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         
+#         l,r=0,len(nums)-1
+        
+#         while(l<=r):
+#             mid=(l+r)//2
+            
+#             if target>nums[mid]:
+#                 l=mid+1
+#             elif target<nums[mid]:
+#                 r=mid-1
+#             else:
+#                 return mid
+        
+#         return l
+    
+    
+    
         l=0
         r=len(nums)
         
@@ -13,5 +29,3 @@ class Solution:
                 l=m+1
         
         return l
-                
-                
