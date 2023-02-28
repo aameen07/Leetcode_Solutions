af3ed379ce02b390e2c205cecff91d1a411d834d
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2023-02-28 23:18:08 +0530

    Time: 37 ms (37.45%), Space: 14 MB (11.76%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -1,47 +1,46 @@
 class Solution:
     def mergeAlternately(self, w1: str, w2: str) -> str:
         
-#         res=""
-        
-#         i=0
-#         j=0
-        
-#         while(i<len(w1) or j<len(w2)):
-            
-#             if i<len(w1):
-#                 res+=w1[i]
-#                 i+=1
-                
-#             if j<len(w2):
-#                 res+=w2[j]
-#                 j+=1
-            
-#         return res   
-    
-    
-        
         res=""
         
         i=0
-        j=0    
+        j=0
         
-        while(i<len(w1) and j<len(w2)):
+        while(i<len(w1) or j<len(w2)):
             
-            if len(res)%2==0:
+            if i<len(w1):
                 res+=w1[i]
                 i+=1
-            else:
+                
+            if j<len(w2):
                 res+=w2[j]
                 j+=1
-                
-        while(i<len(w1)):
-            res+=w1[i]
-            i+=1
             
-        while(j<len(w2)):
-            res+=w2[j]
-            j+=1
+        return res   
+    
+    
         
-
-        return res
+#         res=""
+        
+#         i=0
+#         j=0    
+        
+#         while(i<len(w1) and j<len(w2)):
+            
+#             if len(res)%2==0:
+#                 res+=w1[i]
+#                 i+=1
+#             else:
+#                 res+=w2[j]
+#                 j+=1
+                
+#         while(i<len(w1)):
+#             res+=w1[i]
+#             i+=1
+            
+#         while(j<len(w2)):
+#             res+=w2[j]
+#             j+=1
+        
+#         return res
             
