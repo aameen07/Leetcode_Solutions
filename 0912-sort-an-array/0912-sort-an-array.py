248766f5568e019e2719023d32ff577d1c7961b7
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2023-03-02 22:50:43 +0530

    Time: 1862 ms (56.28%), Space: 22.8 MB (39.07%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,4 +1,43 @@
 class Solution:
+    # def sortArray(self, nums: List[int]) -> List[int]:
+#         sorted_values=self.merge_sort(nums)
+#         return sorted_values
+    
+#     def merge_sort(self,arr):
+        
+#         if len(arr)==1:
+#             return arr
+        
+#         mid = len(arr)//2
+        
+#         left=arr[:mid]
+#         right=arr[mid:]
+        
+#         left_value = self.merge_sort(left)
+#         right_value = self.merge_sort(right)
+        
+#         sortof=[]
+#         l=0
+#         r=0
+        
+#         while l<len(left_value) and r<len(right_value):
+#             if left_value[l]<right_value[r]:
+#                 sortof.append(left_value[l])
+#                 l+=1
+#             else:
+#                 sortof.append(right_value[r])
+#                 r+=1
+#         while l<len(left_value):
+#             sortof.append(left_value[l])
+#             l+=1
+#         while r<len(right_value):
+#             sortof.append(right_value[r])
+#             r+=1
+            
+#         return sortof
+
+# or
+        
     def sortArray(self, nums: List[int]) -> List[int]:
         sorted_values=self.merge_sort(nums)
         return sorted_values
@@ -8,32 +47,26 @@ class Solution:
         if len(arr)==1:
             return arr
         
-        mid = len(arr)//2
+        mid=len(arr)//2
         
-        left=arr[:mid]
-        right=arr[mid:]
+        left_value = self.merge_sort(arr[:mid])
+        right_value = self.merge_sort(arr[mid:])
         
-        left_value = self.merge_sort(left)
-        right_value = self.merge_sort(right)
-        
-        sortof=[]
+        sorted_array = []
         l=0
         r=0
         
         while l<len(left_value) and r<len(right_value):
             if left_value[l]<right_value[r]:
-                sortof.append(left_value[l])
+                sorted_array.append(left_value[l])
                 l+=1
             else:
-                sortof.append(right_value[r])
+                sorted_array.append(right_value[r])
                 r+=1
-        while l<len(left_value):
-            sortof.append(left_value[l])
-            l+=1
-        while r<len(right_value):
-            sortof.append(right_value[r])
-            r+=1
-            
-        return sortof
         
+        sorted_array+=left_value[l:]
+        sorted_array+=right_value[r:]
+        
+        return sorted_array
+
         
