c454af5cf22fb0411975fd70252530ae101b59f6
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2023-03-05 15:15:04 +0530

    Time: 56 ms (40.06%), Space: 16.1 MB (11.87%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -9,26 +9,28 @@ class Solution:
         
 #         return new_s==new_s[::-1]
         
+        # or 
         
+        return [char for char in s.lower() if char.isalnum()] == [char for char in s[::-1].lower() if char.isalnum()]
         
-        s=s.lower()
+#         s=s.lower()
 
-        l=0
-        r=len(s)-1
+#         l=0
+#         r=len(s)-1
         
-        while(l<=r):
+#         while(l<=r):
             
-            while l <r and not s[l].isalnum():
-                l+=1
-            while l <r and not s[r].isalnum():
-                r-=1
-            if s[l]!=s[r]:
-                return False
+#             while l<r and not s[l].isalnum():
+#                 l+=1
+#             while l<r and not s[r].isalnum():
+#                 r-=1
+#             if s[l]!=s[r]:
+#                 return False
             
-            l+=1
-            r-=1
+#             l+=1
+#             r-=1
     
-        return True
+#         return True
 
 
     
