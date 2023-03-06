17f36a08dcf4f49bb5fa612124a41b0fa04552b4
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2023-03-06 22:29:52 +0530

    Time: 39 ms (92.46%), Space: 14.6 MB (67.78%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,26 +1,23 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         
-#         s=s.lower()
-#         new_s = ""
-#         for i in s:
-#             if i.isalnum():
-#                 new_s+=i
+        s=s.lower()
+        new_s = ""
+        for i in s:
+            if i.isalnum():
+                new_s+=i
         
-#         return new_s==new_s[::-1]
+        return new_s==new_s[::-1]
         
         # or 
         
-        return [char for char in s.lower() if char.isalnum()] == [char for char in s[::-1].lower() if char.isalnum()]
-        
 #         s=s.lower()
-
 #         l=0
 #         r=len(s)-1
         
-#         while(l<=r):
+#         while(l<r):
             
-#             while l<r and not s[l].isalnum():
+#             while l<r and  not s[l].isalnum():
 #                 l+=1
 #             while l<r and not s[r].isalnum():
 #                 r-=1
@@ -32,7 +29,9 @@ class Solution:
     
 #         return True
 
-
+            # or
+    
+    # return [char for char in s.lower() if char.isalnum()] == [char for char in s[::-1].lower() if char.isalnum()]
     
     
     
