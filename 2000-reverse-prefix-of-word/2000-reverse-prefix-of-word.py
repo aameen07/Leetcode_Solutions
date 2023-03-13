9d8fa35dbb9776aefcc20c9024e5e1eda1c5e5b5
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2023-03-13 23:24:20 +0530

    Time: 27 ms (88.61%), Space: 13.8 MB (54.82%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.py
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.py
@@ -4,21 +4,32 @@ class Solution:
         a=""
         for r in range(len(word)):
             if word[r]==ch:
-                a+=word[:r+1]
-                a=self.reverse(a)+word[r+1:]
+                a=word[:r+1][::-1]+word[r+1:] 
+                # a=a[::-1]+word[r+1:]        
                 return a
+        
         return word
         
+#         for r in range(len(word)):
+#             if word[r]==ch:
+#                 word+=word[:r+1]
+#                 a=self.reverse(a)+word[r+1:]
+#                 return a
+#         return word
+        
     
-    def reverse(self,a):
-        b=list(a)
-        l=0
-        r=len(b)-1
-        while(l<r):
-            c=b[l]
-            b[l]=b[r]
-            b[r]=c
-            l+=1
-            r-=1
+#     def reverse(self,a):
+#         b=list(a)
+#         l=0
+#         r=len(b)-1
+#         while(l<r):
+#             c=b[l]
+#             b[l]=b[r]
+#             b[r]=c
+#             l+=1
+#             r-=1
                
-        return "".join(b)
+#         return "".join(b)
+    
+    
+    
