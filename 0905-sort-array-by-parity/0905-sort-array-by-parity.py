5f1209cc8c9c90d1dc12d3141c94cb96c4c2272f
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2023-03-14 22:25:29 +0530

    Time: 77 ms (83.65%), Space: 14.7 MB (49.83%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.py
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.py
@@ -1,15 +1,28 @@
 class Solution:
     def sortArrayByParity(self, nums: List[int]) -> List[int]:
         
-        l=0
-        for r in range(1,len(nums)):
+#         l=0
+#         for r in range(1,len(nums)):
             
-            if (nums[l]%2==0):
-                l+=1
-            elif nums[l]%2!=0 and nums[r]%2==0:
-                nums[l],nums[r]=nums[r],nums[l]
-                l+=1
+#             if (nums[l]%2==0):
+#                 l+=1
+            
+#             elif nums[l]%2!=0 and nums[r]%2==0:
+#                 nums[l],nums[r]=nums[r],nums[l]
+#                 l+=1
                 
-        return nums
+#         return nums
         
         # we try to get the left as odd for that we used the if condition and for getting the swap condition possible we used the elif condition
+        
+        
+        l,r=0,0
+        
+        while(r<len(nums)):
+            if nums[r]%2==0:
+                nums[l],nums[r]=nums[r],nums[l]
+                l+=1
+            r+=1
+        
+        return nums
+        
