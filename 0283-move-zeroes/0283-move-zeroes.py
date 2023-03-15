1a58ce45d6cb54f753a880d2fe630a2f89411f1b
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2023-03-15 23:03:26 +0530

    Time: 173 ms (60.15%), Space: 15.5 MB (53.08%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -4,18 +4,54 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         
-        index=0
-        for i in nums:
-            if i!=0:
-                nums[index]=i
-                index+=1
-        
-        for j in range(index,len(nums)):
-            nums[j]=0
-        
+        i=0 #zeros
+        j=0 #nonzeros
+        while (j<len(nums)):
             
-            
-                
+            if nums[j]!=0:
+                nums[i],nums[j]=nums[j],nums[i]
+                i+=1
+            j+=1
+                   
         return nums
+        
+        
+        
+        
+        
+#         index=0
+#         for i in nums:
+#             if i!=0:
+#                 nums[index]=i
+#                 index+=1
+        
+#         for j in range(index,len(nums)):
+#             nums[j]=0
+            
+#         return nums
     
         # we'll use index and try to find non zero values and we put them in index postion then increment index and in the end we just put zeros in the remaining postions.    
+        
+        
+        
+        
+#         index = 0 
+#         for i in nums:
+#             if i != 0:                  #we check if the item is non-zero value
+#                 nums[index]=i
+#                 index+=1
+#         for j in range(index, len(nums)):       #in this loop we're putting zero in the remaining  
+#                                                 #ending length of the loop
+#             nums[j]=0                   
+        
+#         return nums
+
+
+        # if len(nums)==0: return nums      #simply checks if the list is empty
+        # for i in nums:
+        #     if i == 0:
+        #         nums.remove(i)         #we remove it and append it in end
+        #         nums.append(i)
+        #     else:
+        #         continue       # if its not 0 value in the list we continue
+        # return nums
