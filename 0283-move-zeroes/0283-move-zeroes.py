fe6cbb97d636bfce50a6e307e377049d2f3683bf
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2023-03-15 22:46:16 +0530

    Time: 163 ms (87.84%), Space: 15.5 MB (53.08%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,23 +3,20 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        index = 0 
+        
+        index=0
         for i in nums:
-            if i != 0:                  #we check if the item is non-zero value
+            if i!=0:
                 nums[index]=i
                 index+=1
-        for j in range(index, len(nums)):       #in this loop we're putting zero in the remaining  
-                                                #ending length of the loop
-            nums[j]=0                   
         
+        for j in range(index,len(nums)):
+            nums[j]=0
+        
+            
+            
+                
         return nums
-
-
-        # if len(nums)==0: return nums      #simply checks if the list is empty
-        # for i in nums:
-        #     if i == 0:
-        #         nums.remove(i)         #we remove it and append it in end
-        #         nums.append(i)
-        #     else:
-        #         continue       # if its not 0 value in the list we continue
-        # return nums
+    
+        # we're using i to catch zeros in the nums and we're using j to catch non zero values and then we just swap them and move ahead. 
+    
