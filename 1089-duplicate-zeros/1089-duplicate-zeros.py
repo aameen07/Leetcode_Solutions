409442b1516593081ecd735176862017646b4cf4
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2023-03-19 15:47:03 +0530

    Time: 76 ms (46.79%), Space: 14.6 MB (97.06%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.py
+++ 1089-duplicate-zeros/1089-duplicate-zeros.py
@@ -4,12 +4,39 @@ class Solution:
         Do not return anything, modify arr in-place instead.
         """
         
-        i=0
-        while(i<len(arr)):
+        # i=0
+        # while(i<len(arr)):
+        #     if arr[i]==0:
+        #         arr.pop()
+        #         arr.insert(i,0)
+        #         i+=1
+        #     i+=1
+        
+        
+        zeros=0
+        for a in arr:
+            if a==0:
+                zeros+=1
+        
+        i=len(arr)-1
+        j=len(arr)-1+zeros
+        
+        while(i!=j):
+            self.insert(arr,i,j)
             if arr[i]==0:
-                arr.pop()
-                arr.insert(i,0)
-                i+=1
-            i+=1
+                j-=1
+                self.insert(arr,i,j)
+            i-=1
+            j-=1
         
-        
+        return arr
+    
+
+    def insert(self,arr,i,j):
+        if j<len(arr):
+            arr[j]=arr[i]
+            
+            
+            
+            
+            
