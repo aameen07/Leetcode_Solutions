53bd022b445d77940fb0edc024e237b0d3748529
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2023-03-23 22:48:57 +0530

    Time: 38 ms (53.13%), Space: 13.9 MB (50.94%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,13 +1,7 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
         
-#         if n==1:
-#             return True
-        
-#         if n//10==0:
-#             return False
         rem=set()
-        
         while(n!=1):
             total=sum([int(i)**2 for i in str(n)])
             if total in rem:
@@ -17,3 +11,4 @@ class Solution:
         
         else:
             return True
+        
