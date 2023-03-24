a8cbe2796b2308da158702df213a1651169fd65e
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2023-03-25 00:23:23 +0530

    Time: 814 ms (88.00%), Space: 26.8 MB (79.80%) - LeetHub

--- 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.py
+++ 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.py
@@ -9,16 +9,11 @@ class Solution:
         while l<=r:
             if nums[l]+nums[r] <= target:
                 c+=pow(2,r-l,mod)
+                # c+=(2**(r-l))%mod
                 l+=1
             else:
                 r-=1
-        # while l<=r:
-        #     if nums[l]+nums[r] > target:
-        #         r-=1
-        #     else:
-        #         c+=pow(2,r-l,mod)
-        #         l+=1            
-        
+                
         return c%mod
     
     
