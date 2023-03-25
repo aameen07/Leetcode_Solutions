1ad84b3f4648a18968782c4fa0544a912fe21bc3
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2023-03-25 13:50:23 +0530

    Time: 468 ms (60.03%), Space: 20.8 MB (62.15%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.py
+++ 0881-boats-to-save-people/0881-boats-to-save-people.py
@@ -4,15 +4,26 @@ class Solution:
         people.sort()
         l=0
         r=len(people)-1
-        c=0
-        
+        c=0        
         while(l<=r):
-            
             if people[l]+people[r]<=limit:
                 l+=1
                 r-=1
             else:
                 r-=1
             c+=1
-        
         return c
+    
+    
+    # or
+    
+    
+        # people.sort(reverse=True)
+        # print(people)
+        # i, j = 0, len(people) - 1
+        # print(i,j)
+        # while i <= j:
+        #     if people[i] + people[j] <= limit: j -= 1
+        #     i += 1
+        #     print(i,j)
+        # return i
