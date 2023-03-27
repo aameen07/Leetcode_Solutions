018fa0e0d93ca6e2fa8df2e76bfbb28a29ad673b
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2023-03-27 22:56:15 +0530

    Time: 2571 ms (28.36%), Space: 17.5 MB (92.75%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -4,10 +4,8 @@ class Solution:
         nums.sort()
         for i in range(len(nums)-2):
             l,r=i+1,len(nums)-1
-            
             while(l<r):
                 s=nums[i]+nums[l]+nums[r]
-                
                 if s>0:
                     r-=1
                 elif s<0:
@@ -17,10 +15,12 @@ class Solution:
                         hashset.add((nums[i],nums[l],nums[r]))
                     l+=1
                     r-=1
-                    
-                    
         return hashset
-            
+        
+        # or
+        
+        
+        
         
         
         
