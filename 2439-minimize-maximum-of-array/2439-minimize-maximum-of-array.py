b9e5a4d4bb5136234da4bb6c1bcd861be392d21a
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2023-04-05 23:19:29 +0530

    Time: 830 ms (61.88%), Space: 26.8 MB (39.46%) - LeetHub

--- 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.py
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.py
@@ -2,11 +2,11 @@ class Solution:
     def minimizeArrayValue(self, nums: List[int]) -> int:
         
         res=0
-        total=0
+        prefixsum=0
         
         for i, num in enumerate(nums):
-            total+=num
-            res= max(res,math.ceil(total/(i+1)))
+            prefixsum+=num
+            res= max(res,math.ceil(prefixsum/(i+1)))
         
         return res
             
