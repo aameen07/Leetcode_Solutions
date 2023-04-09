c8bc2253d7d2e57dd8c91bb20b18f23d70898a6b
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2023-04-09 14:44:22 +0530

    Time: 275 ms (27.80%), Space: 27.1 MB (35.17%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -5,13 +5,14 @@ class Solution:
         total=0
         res=len(nums)+1
         for r in range(len(nums)):
+            
             total+=nums[r]
             
-            while(total>=target):
-                res = min(res,r-l+1)
+            while total>=target:
+                res=min(res,r-l+1)
                 total-=nums[l]
                 l+=1
-        
-        return res if res!=len(nums)+1 else 0
                 
-            
+        return res if res!=len(nums)+1 else 0
+        
+        
