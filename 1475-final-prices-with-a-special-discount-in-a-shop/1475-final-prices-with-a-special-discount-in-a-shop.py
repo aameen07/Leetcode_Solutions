e11252c0245f97240d5cb1927554dff14f71b2da
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2023-04-18 23:14:30 +0530

    Time: 59 ms (27.46%), Space: 14 MB (51.10%) - LeetHub

---
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
@@ -0,0 +1,15 @@
+class Solution:
+    def finalPrices(self, p: List[int]) -> List[int]:
+        
+        res=[]
+        dic=[]
+        
+        for i in range (len(p)):
+            
+            while res and p[i]<=p[res[-1]]:
+                p[res.pop()]-=p[i]
+            
+            res.append(i)
+        
+        return p
+        
