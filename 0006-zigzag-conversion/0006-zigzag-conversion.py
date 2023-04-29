680b8689a4a07eabbec9efca17ae90d76eb26bd4
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2023-04-29 23:57:40 +0530

    Time: 73 ms (29.00%), Space: 16.5 MB (9.62%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -5,14 +5,14 @@ class Solution:
             return s
         
         arr=[""]*numRows
-        direction=False
+        direction=False         # False means When going DOWN , True means when going UP
         row = 0
         for i in s:
             arr[row]+=i
-            if row == numRows-1:
-                direction = True
-            elif row == 0:
-                direction = False
+            if row == numRows-1:            # yahan se
+                direction = True            #
+            elif row == 0:            # 
+                direction = False            # yahan tk direction change kr rhe hai sirf
             
             if (direction == False):
                 row+=1
