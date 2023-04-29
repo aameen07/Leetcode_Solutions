7807be80bd41f2c9acfab6ff05fe872c7d9e1c1e
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2023-04-30 00:32:25 +0530

    Time: 323 ms (17.22%), Space: 19 MB (5.22%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -0,0 +1,24 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        
+#         [1,2,-1,3,7,-4]
+#         3
+
+
+        psum=0
+        d={0:1}
+        count=0
+        
+        for i, val in enumerate(nums):
+            psum+=val
+            if psum-k in d:
+                count+=d[psum-k]
+    
+            d[psum]=d.get(psum,0)+1
+            
+        print(d)
+        return count
+        
+        
+            
+            
