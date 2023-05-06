59eb7686926e3a2a623ec0dfa7f55ee4f236b7b5
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2023-05-06 23:40:41 +0530

    Time: 636 ms (98.59%), Space: 29.7 MB (8.18%) - LeetHub

--- 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.py
+++ 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.py
@@ -1,19 +1,39 @@
 class Solution:
     def numSubseq(self, nums: List[int], target: int) -> int:
         
+#         nums.sort()
+#         l=0
+#         r=len(nums)-1
+#         c=0
+#         mod=10**9+7
+#         while l<=r:
+#             if nums[l]+nums[r] <= target:
+#                 c+=pow(2,r-l,mod)
+#                 # c+=(2**(r-l))%mod
+#                 l+=1
+#             else:
+#                 r-=1               
+        
+#         return c % mod
+    
+        
+        # or
+        
+        
+        i=0
+        j=len(nums)-1
         nums.sort()
-        l=0
-        r=len(nums)-1
         c=0
         mod=10**9+7
-        while l<=r:
-            if nums[l]+nums[r] <= target:
-                c+=pow(2,r-l,mod)
-                # c+=(2**(r-l))%mod
-                l+=1
+        pows=[1]*len(nums)
+        for k in range(1,len(nums)):
+            pows[k]=(pows[k-1]*2)%mod
+        while i<=j:
+            if nums[i]+nums[j]<=target:
+                c+=pows[j-i]%mod
+                i+=1
             else:
-                r-=1               
-        
+                j-=1
         return c % mod
         
-    
+        
