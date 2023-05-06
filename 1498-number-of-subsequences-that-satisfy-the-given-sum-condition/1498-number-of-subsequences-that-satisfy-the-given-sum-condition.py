3563b13192000c3ea76f89b4fb15b846841a45ce
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2023-05-06 23:41:21 +0530

    Time: 660 ms (97.83%), Space: 29.7 MB (8.18%) - LeetHub

--- 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.py
+++ 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.py
@@ -16,18 +16,18 @@ class Solution:
         
 #         return c % mod
     
-        
         # or
         
-        
-        i=0
         j=len(nums)-1
         nums.sort()
         c=0
         mod=10**9+7
+        
         pows=[1]*len(nums)
         for k in range(1,len(nums)):
             pows[k]=(pows[k-1]*2)%mod
+        
+        i=0
         while i<=j:
             if nums[i]+nums[j]<=target:
                 c+=pows[j-i]%mod
