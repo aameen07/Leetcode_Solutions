fc11e994e42690223050ece84d2c3c801b64a4e6
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2023-05-08 00:02:47 +0530

    Time: 52 ms (14.99%), Space: 16.3 MB (13.20%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
@@ -1,11 +1,23 @@
 class Solution:
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
         
-        students=collections.Counter(students)
-        for sand in sandwiches:
-            if not students[sand]:
-                break
-            students[sand]-=1
+#         students=collections.Counter(students)
+#         for sand in sandwiches:
+#             if not students[sand]:
+#                 break
+#             students[sand]-=1
         
-        return sum(students.values())
+#         return sum(students.values())
+        
+        
+        # or
+        
+        for i,sand in enumerate(sandwiches):
+            if sand in students:
+                students.remove(sand)
+                # print(students)
+            else:
+                return len(sandwiches)-i
+                
+        return 0
         
