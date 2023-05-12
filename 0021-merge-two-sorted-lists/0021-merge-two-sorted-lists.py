0bfa42dd4ed669bb50cc1189818e0095dfe2b2f2
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2023-05-13 00:06:25 +0530

    Time: 60 ms (5.72%), Space: 16.4 MB (7.73%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -6,11 +6,11 @@
 class Solution:
     def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         
-        res=ListNode()
-        cur=res
+        res=ListNode()          #This made a node with 0 val and None .next address
+        cur=res                 #now i create a copy of the head
         
-        while l1 and l2:
-            
+        while l1 and l2:        
+
             if l1.val<l2.val:
                 cur.next=l1
                 l1=l1.next
@@ -21,17 +21,13 @@ class Solution:
                 
             cur=cur.next
         
-        if l1:
-            cur.next=l1
-            l1=l1.next
+        if l1:                  #If any list is not empty even now then we add its link only one time
+            cur.next=l1         #cuz that will add the remaining linked list which got left
             
-        
         if l2:
             cur.next=l2
-            l2=l2.next
         
-        return res.next
-            
-        
+        return res.next         #We give the .next address so that we can avoid that extra node that we 
+                                #added in the start
         
         
