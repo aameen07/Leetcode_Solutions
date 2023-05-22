b0849b64e14358646230b4ce53d7c4af549cc71e
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2023-05-22 23:17:02 +0530

    Time: 443 ms (33.25%), Space: 22.6 MB (26.27%) - LeetHub

--- 1669-merge-in-between-linked-lists/1669-merge-in-between-linked-lists.py
+++ 1669-merge-in-between-linked-lists/1669-merge-in-between-linked-lists.py
@@ -5,25 +5,50 @@
 #         self.next = next
 class Solution:
     def mergeInBetween(self, list1: ListNode, a: int, b: int, list2: ListNode) -> ListNode:
-        curr=list1
-        nxt=list1.next
-        count=1
-        while nxt:
-            if count==a:
-                curr.next=list2
-                break
-            else:
-                curr=curr.next
-                nxt=nxt.next
-                count+=1
         
-        while nxt:
-            if count==b:
-                nxt=nxt.next
-                break
-            else:
-                nxt=nxt.next
-                count+=1
+        
+        
+#         while nxt:
+#             if count==a:
+#                 curr.next=list2
+#                 break
+#             else:
+#                 curr=curr.next
+#                 nxt=nxt.next
+#                 count+=1
+        
+#         while nxt:
+#             if count==b:
+#                 nxt=nxt.next
+#                 break
+#             else:
+#                 nxt=nxt.next
+#                 count+=1
+        
+#         while curr.next:
+#             curr=curr.next
+        
+#         curr.next=nxt
+        
+#         return list1
+    
+    
+        # or
+        
+        
+        curr=nxt=list1
+        count=0
+        
+        while count!=a-1:
+            curr=curr.next
+            nxt=nxt.next
+            count+=1
+        
+        while count!=b+1:
+            nxt=nxt.next
+            count+=1
+        
+        curr.next=list2
         
         while curr.next:
             curr=curr.next
@@ -31,3 +56,15 @@ class Solution:
         curr.next=nxt
         
         return list1
+            
+            
+            
+            
+            
+            
+        
+        
+        
+        
+        
+        
