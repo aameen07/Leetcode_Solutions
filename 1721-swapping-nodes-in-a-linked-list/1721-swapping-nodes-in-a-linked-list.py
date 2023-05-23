91be70eb1170c1299764ae9be7c7fca1249a03a4
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2023-05-23 22:43:34 +0530

    Time: 965 ms (66.82%), Space: 50.7 MB (51.63%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.py
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.py
@@ -6,27 +6,41 @@
 class Solution:
     def swapNodes(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
         
-        curr=head
-        c=0
-        arr=[]
-        while curr:
-            arr.append(curr.val)
-            curr=curr.next
-            c+=1
-        # print(c,arr)
-        arr[k-1],arr[c-k]=arr[c-k],arr[k-1]
-        # print(arr)
+#         curr=head
+#         c=0
+#         arr=[]
+#         while curr:
+#             arr.append(curr.val)
+#             curr=curr.next
+#             c+=1
         
-        curr=head
-        i=0
-        while i!=c:
-            curr.val=arr[i]
-            curr=curr.next
-            i+=1
+#         arr[k-1],arr[c-k]=arr[c-k],arr[k-1]
+        
+#         curr=head
+#         i=0
+#         while i!=c:
+#             curr.val=arr[i]
+#             curr=curr.next
+#             i+=1
+        
+#         return head
+
+                # or
+        
+        curr = head
+        for _ in range(k-1):
+            curr = curr.next
+        first = curr
+        
+        second = head
+        while curr.next:
+            curr = curr.next
+            second = second.next
+        
+        first.val, second.val = second.val, first.val
         
         return head
         
-            
         
         
         
