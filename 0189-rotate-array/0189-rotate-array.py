8fc16612b00a9303ce58333432ff37e1db9c9a8d
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2023-06-08 23:14:52 +0530

    Time: 221 ms (70.32%), Space: 27.7 MB (50.65%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,29 +1,23 @@
-# class Solution:
-#     def rotate(self, nums: List[int], k: int) -> None:
-        # k=k % len(nums)
-        # if k>0:
-        #     nums[:]=nums[-k:]+nums[:-k]
-
-
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
-        n=len(nums)
-        k=k%n
-        self.reverse(nums,0,n-1)
-        self.reverse(nums,0,k-1)
-        self.reverse(nums,k,n-1)
-    
-    def reverse(self, nums, start, end):
-        while(start<end):
-            temp=nums[start]
-            nums[start]=nums[end]
-            nums[end]=temp
-            start+=1
-            end-=1
+        k=k % len(nums)
+        if k>0:
+            nums[:]=nums[-k:]+nums[:-k]
+
+
+# class Solution:     #ye universal solution hai upr wala python specific hai
+#     def rotate(self, nums: List[int], k: int) -> None:
+#         n=len(nums)
+#         k=k%n
+#         self.reverse(nums,0,n-1)
+#         self.reverse(nums,0,k-1)
+#         self.reverse(nums,k,n-1)
+
+#     def reverse(self, nums, start, end):
         
-          
-            
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        
+#         while(start<end):
+#             temp=nums[start]
+#             nums[start]=nums[end]
+#             nums[end]=temp
+#             start+=1
+#             end-=1
