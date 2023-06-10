a33265c8d0ac53d19bc3ae4e5fe2ae61e26cea3d
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2023-06-10 23:24:24 +0530

    Time: 1042 ms (11.20%), Space: 57 MB (19.81%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
@@ -5,24 +5,45 @@
 #         self.next = next
 class Solution:
     def pairSum(self, head: Optional[ListNode]) -> int:
-        
-        slow,fast=head,head
+        cur=head
+        l=[]
         maxVal=0
         
-        while fast and fast.next:
-            slow=slow.next
-            fast=fast.next.next
-            
-        cur,prev=slow,None
         while cur:
-            nxt=cur.next
-            cur.next=prev
-            prev=cur
-            cur=nxt
-
-        while prev:
-            maxVal=max(maxVal,head.val+prev.val)
-            prev=prev.next
-            head=head.next
-
+            l.append(cur.val)
+            cur=cur.next
+        
+        n=len(l)
+        i=0
+        while i<n:
+            maxVal=max(maxVal,l[i]+l[n-1-i])    
+            i+=1
+        
         return maxVal
+        
+        
+        
+        
+        
+        
+        
+#         slow,fast=head,head
+#         maxVal=0
+        
+#         while fast and fast.next:
+#             slow=slow.next
+#             fast=fast.next.next
+            
+#         cur,prev=slow,None
+#         while cur:
+#             nxt=cur.next
+#             cur.next=prev
+#             prev=cur
+#             cur=nxt
+
+#         while prev:
+#             maxVal=max(maxVal,head.val+prev.val)
+#             prev=prev.next
+#             head=head.next
+
+#         return maxVal
