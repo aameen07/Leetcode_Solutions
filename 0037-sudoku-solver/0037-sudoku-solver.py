ec9449e39aef65601d30e00ef93f672b5193e245
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2023-07-05 00:07:38 +0530

    Time: 1037 ms (6.48%), Space: 16.4 MB (59.16%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.py
+++ 0037-sudoku-solver/0037-sudoku-solver.py
@@ -1,9 +1,5 @@
 class Solution:
     def solveSudoku(self, board: List[List[str]]) -> None:
-        """
-        Do not return anything, modify board in-place instead.
-        """
-        
         self.solve(board)
     
     def solve(self,board):
@@ -13,13 +9,13 @@ class Solution:
                     for c in range(1,10):
                         if self.isValid(board,i,j,c):
                             board[i][j] = str(c)
-                            
                             if (self.solve(board) == True):
                                 return True
                             else:
                                 board[i][j] = "."
                     return False
         return True
+    
     
     def isValid(self,board,row,col,c):
         for i in range(0,9):
