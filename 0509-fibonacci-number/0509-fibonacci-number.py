b705f8c1cc7c328be3d91d5bb6ee67425ed6855c
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2023-07-13 22:43:01 +0530

    Time: 761 ms (15.12%), Space: 16.3 MB (25.54%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,13 +1,11 @@
 class Solution:
     def fib(self, n: int) -> int:
         
-        dp = [0]*(n+1) # we are creating dp array of length 1 greater than the length of fibonacci                          series we're supposed to return  
+        if n==0:
+            return 0
+        if n==1:
+            return 1
+        res=self.fib(n-1)+self.fib(n-2)
+        return res
         
-        if n<=1: return n
         
-        dp[1]=1
-        
-        for i in range(2, len(dp)): # here len(dp) is n+1 in size
-            dp[i]=dp[i-1]+dp[i-2]  #we take the previous two values of the dp to get the new value                                      of dp
-            
-        return dp[-1]
