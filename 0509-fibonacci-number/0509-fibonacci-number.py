84ffba0453e06106b0cc5311bb914de465ab0208
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2023-07-15 23:47:09 +0530

    Time: 40 ms (90.12%), Space: 16.4 MB (25.98%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -3,10 +3,10 @@ class Solution:
         def fibn(n,dp):
             if n==0: return 0
             elif n==1: return 1
-            elif dp[n]!=-1: return dp[n]
+            elif dp[n]!=0: return dp[n]
             dp[n]=fibn(n-1,dp)+fibn(n-2,dp)
             return dp[n]
             
-        dp=[-1]*(n+1)
+        dp=[0]*(n+1)
         res=fibn(n,dp)
         return res
