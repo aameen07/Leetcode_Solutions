3abf19f34d20db455d1d26e5e202bab357927a64
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2023-07-17 23:02:14 +0530

    Time: 43 ms (75.47%), Space: 16.3 MB (69.30%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -3,12 +3,22 @@ class Solution:
         
         #0,1,1,2,3,5,8,13,21,34,55
         
-        dp = [0]*(n+1)
-        dp[0] = 1
+#         dp = [0]*(n+1)
+#         dp[0] = 1
         
-        for i in range(1,len(dp)):
-            dp[i] = dp[i-1]+dp[i-2] 
+#         for i in range(1,len(dp)):
+#             dp[i] = dp[i-1]+dp[i-2] 
             
-        return dp[-1]
-            
+#         return dp[-1]
+
+        # or
+    
+        dp=[0]*(n+1)
+        if n==1: return 1
+        dp[1]=1
+        dp[2]=2
+        for i in range(3,n+1):
+            dp[i]=dp[i-1]+dp[i-2]
+        return dp[n]
+
         
