de92e6b735c959cf497d01278e14efa80d553f6d
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2023-07-22 23:33:20 +0530

    Time: 56 ms (28.69%), Space: 16.3 MB (55.38%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -3,10 +3,10 @@ class Solution:
         m=matrix
         
         for i in range (len(m)):
-            for j in range(i,len(m)):
-                # 
-                m[i][j],m[j][i]=m[j][i],m[i][j]
+            for j in range(i+1,len(m)):
+                
+                m[i][j],m[j][i]=m[j][i],m[i][j] # pehla column ab pehli row ban jaega 
         
         for i in range(len(m)):
-            m[i] = m[i][::-1]
+            m[i] = m[i][::-1]   # hum har ek row ko reverse kr denge and we get ans
         
