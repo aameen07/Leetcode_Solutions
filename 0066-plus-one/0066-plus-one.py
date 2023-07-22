20fd2d1ba7424378cb1fc1a3252a16b721df4308
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2023-07-22 23:51:55 +0530

    Time: 48 ms (61.65%), Space: 16.3 MB (70.08%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,12 +1,22 @@
 class Solution:
-    def plusOne(self, d: List[int]) -> List[int]:
+    def plusOne(self, digits: List[int]) -> List[int]:
         
-        if d[-1] != 9:
-            d[-1]+=1
-            return d
-        elif len(d)==1 and d[-1] == 9:
-            return [1,0]
-        else:
-            d[-1]=0
-            d[0:-1] = self.plusOne(d[0:-1])
-            return d
+        # if d[-1] != 9:
+        #     d[-1]+=1
+        #     return d
+        # elif len(d)==1 and d[-1] == 9:
+        #     return [1,0]
+        # else:
+        #     d[-1]=0
+        #     d[0:-1] = self.plusOne(d[0:-1])
+        #     return d
+        
+        # or
+        
+        b=""
+        for i in digits:
+            b+=str(i)
+        b=str(int(b)+1)
+        c=[int(j) for j in b]
+        return c
+                
