e038f4ae61ae2a593c8d0ee7b2e8d056b9ada2aa
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2023-08-02 23:01:32 +0530

    Time: 49 ms (83.55%), Space: 16.4 MB (91.96%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -5,9 +5,11 @@ class Solution:
         return res
     
     def dfs(self,nums,path,res):
+        
         if not nums:
             res.append(path)
+            
         for i in range(len(nums)):
+            
             self.dfs(nums[:i]+nums[i+1:],path+[nums[i]],res)
-        
         
