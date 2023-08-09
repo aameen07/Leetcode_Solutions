c5504ec6b05a6486064d38944ef5071b14a2f76f
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2023-08-09 23:39:31 +0530

    Time: 429 ms (86.55%), Space: 17.5 MB (92.81%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -7,10 +7,9 @@ class Solution:
         jump=nums[0]
         
         for i in range(1,len(nums)):
-            if jump==0:
+            if i>jump:
                 return False
-            jump-=1
-            jump=max(jump,nums[i])
+            jump=max(jump,i+nums[i])
         return True
             
         
