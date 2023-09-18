262c65d8e3fecb9d3b748a77f2ffa89b13c1adbd
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2023-09-18 23:13:43 +0530

    Time: 105 ms (67.48%), Space: 16.6 MB (94.25%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
@@ -1,10 +1,10 @@
 class Solution:
     def kWeakestRows(self, nums: List[List[int]], k: int) -> List[int]:
         arr=[]
-        for i, val in enumerate(nums):
-            arr.append([sum(val),i])
-            
-        arr.sort(key=lambda c : (c[0],c[1]))
         
-        return [i for _, i in arr[:k]]
+        for i, val in enumerate(nums):
+            arr.append([sum(val),i])    #we take the pairs of sum and index
+            
+        arr.sort(key=lambda c : (c[0],c[1]))   # it will sort first acc to sum and if sum is equal then acc to                                                  indexes in that particular case
+        return [i for _, i in arr[:k]]          # we need only k indexes and they are already in weakest to                                                     strongest form now
         
