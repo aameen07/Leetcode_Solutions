ff38ea38132f812535cadc69661bb44c6409b996
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2024-01-14 21:06:17 +0530

    Time: 181 ms (47.27%), Space: 18.1 MB (5.52%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -1,18 +1,20 @@
 class Solution:
     def minSteps(self, s: str, t: str) -> int:
-#         dic={}
-#         for i in s:
-#             if i in dic:
-#                 dic[i]+=1
-#             else:
-#                 dic[i]=1
-#         for j in t:
-#             if j in dic and dic[j]>0:
-#                 dic[j]-=1
-#         return sum(dic.values())
+        dic={}
+        for i in s:
+            if i in dic:
+                dic[i]+=1
+            else:
+                dic[i]=1
+        for j in t:
+            if j in dic and dic[j]>0:
+                dic[j]-=1
+        return sum(dic.values())
         
-        cs=collections.Counter(s)
-        ct=collections.Counter(t)
+    # or
+    
+#         cs=collections.Counter(s)
+#         ct=collections.Counter(t)
         
-        diff = cs-ct
-        return sum(diff.values())
+#         diff = cs-ct
+#         return sum(diff.values())
