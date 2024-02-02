00ad58b375c67ede6e54f76186ec138eda9a0374
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2024-02-02 19:32:02 +0530

    Time: 47 ms (76.47%), Space: 17 MB (59.36%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -4,9 +4,15 @@ class Solution:
         for i in s:
             if i in d: d[i]+=1
             else: d[i]=1
+                
         for j in t:
             if j in d: d[j]-=1
             else: return False
+        print(d.items())
         for k,v in d.items():
             if v!=0: return False
+        
         return True
+    
+    
+# 
