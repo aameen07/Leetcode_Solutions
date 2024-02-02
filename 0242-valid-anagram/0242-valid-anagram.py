8b13a0a3ea85298544f873930c28368a1659b8f6
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2024-02-02 19:33:03 +0530

    Time: 45 ms (83.00%), Space: 16.9 MB (66.03%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,5 +1,8 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        
+        if len(s)!=len(t): return False
+        
         d={}
         for i in s:
             if i in d: d[i]+=1
@@ -8,7 +11,7 @@ class Solution:
         for j in t:
             if j in d: d[j]-=1
             else: return False
-        print(d.items())
+
         for k,v in d.items():
             if v!=0: return False
         
