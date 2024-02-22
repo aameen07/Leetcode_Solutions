ce5e982c707eb4352b7fd1571c3d5fadbab253ba
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2024-02-22 16:22:46 +0530

    Time: 163 ms (13.42%), Space: 18 MB (83.38%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -2,27 +2,9 @@ class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         d={}
         for i in nums:
-            if i in d:
-                d[i]+=1
-            else:
+            if i not in d:
                 d[i]=1
-        n=len(nums)
-        for i in d:
-            if d[i]>=(n/2):
-                return i
-
-
-#                   OR
-
-# class Solution:
-#     def majorityElement(self, nums: List[int]) -> int:
-#         d = {}
-#         for n in nums:
-#             if n in d:
-#                 d[n] += 1
-#             else:
-#                 d[n] = 1
-        
-#         return max(d, key=d.get)
-        
-    
+            else:
+                d[i]+=1
+                
+        return max(d, key=d.get)
