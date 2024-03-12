05fe00d1de77ffa9784040337eea20b834451038
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2024-03-12 15:02:44 +0530

    Time: 33 ms (69.65%), Space: 16.5 MB (67.02%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.py
+++ 0290-word-pattern/0290-word-pattern.py
@@ -1,19 +1,37 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
         
-        p=list(pattern)
-        s=s.split()
-        d={}
+#         p=list(pattern)
+#         s=s.split()
+#         d={}
         
-        if len(s)!=len(p): return False
-        if len(set(s))!=len(set(p)): return False
+#         if len(s)!=len(p): return False
+#         if len(set(s))!=len(set(p)): return False
         
-        for i , val in enumerate(p):
-            if val in d and d[val]!=s[i]:
-                return False
-            else:
-                d[val]=s[i]
+#         for i , val in enumerate(p):
+#             if val in d and d[val]!=s[i]:
+#                 return False
+#             else:
+#                 d[val]=s[i]
                 
+#         return True
+
+# or
+
+
+        words = s.split(" ")
+        if len(pattern) != len(words):
+            return False
+        charToWord = {}
+        wordToChar = {}
+        
+        for c, w in zip(pattern, words):
+            if c in charToWord and charToWord[c] != w:
+                return False
+            if w in wordToChar and wordToChar[w] != c:
+                return False
+            charToWord[c] = w
+            wordToChar[w] = c
         return True
 
         
