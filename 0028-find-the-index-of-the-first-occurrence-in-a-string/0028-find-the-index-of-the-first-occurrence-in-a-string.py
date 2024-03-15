c67ac7eb19289582c7c34d9a248b3ec2a5aaf93b
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2024-03-15 14:29:10 +0530

    Time: 27 ms (93.63%), Space: 16.4 MB (91.98%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -3,6 +3,7 @@ class Solution:
         
         if h==n:
             return 0
+        
         l=0
         for r in range(len(n)-1,len(h)):
             if h[l:r+1]==n:
@@ -10,5 +11,4 @@ class Solution:
             else:
                 l+=1
         return -1
-        
         
