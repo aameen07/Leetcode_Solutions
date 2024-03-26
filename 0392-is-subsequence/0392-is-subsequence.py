d05b90e92e92030126798c3916afbc083236d4e1
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2024-03-26 22:25:52 +0530

    Time: 48 ms (5.23%), Space: 16.6 MB (40.39%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -9,7 +9,5 @@ class Solution:
         
         return len(s)==count
     
-# This checks if the 
-
-
-        
+# Humne ye check kia ke kys s string t string mei seq mei hai available uske liye s ki length check ki everytime and also ye check kia ke wo kya seq mei mil rha hai ya nhi 
+# and end mei check kia ke kya saare characters s ke t mei the ya nhi seq mei tbhi un dono ka count same aaega
