271bd3960ac30e104d1e237d1b4b4de5916da910
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2024-05-07 15:15:05 +0530

    Time: 83 ms (85.02%), Space: 20.9 MB (83.34%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -3,3 +3,5 @@ class Solution:
         num=Counter(nums)
         return sorted(num, key=num.get, reverse=True)[:k]
     
+    
+# The counter gets the nums values as keys with their count as the values. Then in sorted we take that dic and give num.get so that it chooses values of dic as the basis of sorting and then reverse the sorted array. now [:k] this was given to slice and return only the k elements of the array
