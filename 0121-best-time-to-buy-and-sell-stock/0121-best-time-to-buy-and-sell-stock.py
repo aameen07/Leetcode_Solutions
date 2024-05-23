75a8ff5576cfbaa591bd69d5ec9c762d50193a12
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2024-05-23 13:29:52 +0530

    Time: 740 ms (61.31%), Space: 27.4 MB (33.88%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -3,16 +3,14 @@ class Solution:
         
         maxProfit=0
         l=prices[0]
-        # if len(prices)==2 and prices[l+1]>prices[l]:
-        #     return prices[l+1]-prices[l]
         
         for r in range(1,len(prices)):
-            if prices[r]<=l:
+            
+            if prices[r]<l:
                 l=prices[r]
                 continue
             else:
                 maxProfit=max(maxProfit,prices[r]-l)
-                # print(maxProfit)
-        
+            
         return maxProfit
-        
+    
