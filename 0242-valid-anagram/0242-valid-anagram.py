8cc342429b8f565a285c17f40a04f486b6ed36b4
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2024-06-10 16:05:30 +0530

    Time: 46 ms (72.28%), Space: 16.8 MB (97.62%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,24 +1,21 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         
-        if len(s) != len(t): return False
+        if len(s)!=len(t): return False
         
         d={}
         for i in s:
-            if i in d:
-                d[i]+=1
-            else:
-                d[i]=1
-        
+            if i in d: d[i]+=1
+            else: d[i]=1
+
         for j in t:
-            if j in d:
-                d[j]-=1
-            else:
-                return False
-            
+            if j in d: d[j]-=1
+            else: return False
+
         for k,v in d.items():
             if v!=0: return False
-            
+
         return True
-                
-        
+    
+    
+# we get the count of one string and subtract the count of the other string from the same dictionary and then we check if that dic count, if there is a count then its not an anagram otherwise it is.
