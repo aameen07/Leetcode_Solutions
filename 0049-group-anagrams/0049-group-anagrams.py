5ae252db79570dc0f7302daead3cb025522b0994
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2024-06-11 18:26:24 +0530

    Time: 91 ms (50.76%), Space: 19.7 MB (76.42%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,16 +1,17 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         
-        d={}
+        dic={}
         
         for i in strs:
+            sorted_str = "".join(sorted(i))
             
-            sorted_string = "".join(sorted(i))
+            if sorted_str not in dic:
+                dic[sorted_str] = []
             
-            if sorted_string not in d:
-                d[sorted_string] = []
+            dic[sorted_str].append(i)
             
-            d[sorted_string].append(i)
-        
-        return (d.values())
-        
+        return list(dic.values())
+    
+
+# We simply used the idea of sorted string this helped in finding the commonality in the anagrams. Then we simply stacked them under that sorted string key in a list and in the end returned all the values together in list format. 
