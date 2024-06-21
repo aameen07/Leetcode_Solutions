df4084b8b2ecb496c6b959fece4cb12d68f32e9b
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2024-06-21 22:09:03 +0530

    Time: 256 ms (92.53%), Space: 25.6 MB (78.67%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,19 +1,19 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         
-        res = [1]*len(nums)
+        res=[1]*len(nums)
         
-        prefix = 1
+        prefix=1
         for i in range(len(nums)):
-            res[i] = prefix
-            prefix *= nums[i]
+            res[i]=prefix
+            prefix*=nums[i]
         
         postfix=1
         for i in range(len(nums)-1,-1,-1):
-            res[i] *= postfix
-            postfix *= nums[i]
-            
+            res[i]*=postfix
+            postfix*=nums[i]
+        
         return res
-        
-        
+    
+    
 # We simply first entered the prefix multiplicated values in the result array from the second postion then we took the postfix multiplicated values and multiplied them with the result array values from the end postion going backwards this gave us the result array which we needed according to the question.
