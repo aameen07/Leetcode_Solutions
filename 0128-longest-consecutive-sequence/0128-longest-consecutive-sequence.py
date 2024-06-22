eb40af3543722fdfb68a5bbc9d7c2de263e26737
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2024-06-22 21:45:04 +0530

    Time: 4231 ms (29.58%), Space: 31.7 MB (69.07%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -14,3 +14,4 @@ class Solution:
         
         return longest
         
+# We have to take a integer that holds our result and a resettable integer that holds the current Consecutive Sequence length. so we make a set and then go through nums and then check if that value is the starting of a new sequence and then we check if the next element is there or not and keep checking that and later we check which is maximum the length or the last longest. and give longest in the end
