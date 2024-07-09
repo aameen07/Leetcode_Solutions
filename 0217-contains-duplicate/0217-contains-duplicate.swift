eaa90a70589374da2ca34307364b80943f637066
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2024-07-09 22:45:58 +0530

    Time: 244 ms (76.10%), Space: 19.8 MB (81.44%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.swift
+++ 0217-contains-duplicate/0217-contains-duplicate.swift
@@ -1,13 +1,5 @@
 class Solution {
     func containsDuplicate(_ nums: [Int]) -> Bool {
-        var dic = [Int:Int]()
-        for i in nums {
-            if dic[i] == nil {
-                dic[i] = 1
-            } else {
-                return true
-            }
-        }
-        return false
+        return nums.count != Set(nums).count
     }
 }
