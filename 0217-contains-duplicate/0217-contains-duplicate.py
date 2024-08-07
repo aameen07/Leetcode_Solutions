4253ad843f6f25c2b47739a984f0d77badb1f14e
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2024-08-07 12:13:33 +0530

    Time: 410 ms (80.25%), Space: 32 MB (45.18%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,20 +1,6 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-
+        
         return len(nums) != len(set(nums))
     
-        # or
-        # return True if len(set(nums)) < len(nums) else False
-        # or
         
-        # d={}
-        # for i in nums:
-        #     if i not in d:
-        #         d[i]=1
-        #     else:
-        #         return True
-        # return False
-        
-        
-# Just need to return true if any value is more than once.
-# We can also just compare normal len and set len.
