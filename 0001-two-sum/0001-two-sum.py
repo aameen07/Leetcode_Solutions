b46e8634a3b52fb6069e673ce6bb54db8f551500
Abdullah Aameen <71563042+aameen07@users.noreply.github.com>
2024-08-07 12:30:14 +0530

    Time: 50 ms (94.47%), Space: 17.7 MB (51.00%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,10 +1,17 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
+        # for i in range(len(nums)-1):
+        #     for j in range(i+1,len(nums)):            #O(n**2)
+        #         if nums[i]+nums[j] == target:
+        #             return [i,j]
+                 
+            # or
+        
         d={}
         for i in range(len(nums)):
-            if target-nums[i] in d:
-                return [d[target-nums[i]],i]
+            if target - nums[i] in d:                   #O(n)
+                return [d[target - nums[i]],i]
             d[nums[i]]=i
-                       
-        
+
+# hum dic banaenge jaise jaise aage iterate krenge nums mei and hum check krenge target mei se abhi ka element subtract krke ke jo res aega wo hai kya dic mei and ni hoga to abhi wale element ko key bana kr uska index value pr dal denge and aage check krenge until we find the target.
